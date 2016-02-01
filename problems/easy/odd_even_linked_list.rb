67ff36d376956ab8e84f1cd79c7bc42a6ad413eb
Zhemin Mi <forevermzm00@gmail.com>
2016-02-01 09:17:12 -0800

    https://leetcode.com/problems/odd-even-linked-list/

---
+++ problems/easy/odd_even_linked_list.rb
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} head
+# @return {ListNode}
+def odd_even_list(head)
+    if head
+        even_head = head.next
+        even_node = even_head
+        odd_node = head
+        while even_node and even_node.next
+            odd_node.next = even_node.next
+            even_node.next = odd_node.next.next
+            odd_node = odd_node.next
+            even_node = even_node.next
+        end
+        odd_node.next = even_head
+    end
+    return head
+end
